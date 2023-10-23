5d324a3a15b164dea441b88a891c892631cd74b0
answhdgkr95 <49180647+answhdgkr95@users.noreply.github.com>
2023-10-23 11:45:35 +0900

    first Commit

---
+++ eSonicEcm/src/test/java/com/eSonic/ecm/ESonicEcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eSonic.ecm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESonicEcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
